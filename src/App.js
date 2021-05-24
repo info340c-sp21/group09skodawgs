d9b3168040af17e8d3ca37403512d19e82f4346c
Jade D'Souza <jadedsou@uw.edu>
2021-05-23 19:58:27 -0700

    react app base code in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
 import App from './App';
 
-export function App () {
+class App extends Component {
+    render() {
+    return(<div class="landing-text">
+        <h1 class="logo text">
+            <span class='drinkingchoices'>Drink Choices</span>
+        </h1>
+        <h3 class="slogan text">
+            <span>Woop Woop!</span><span></span>
+        </h3>
+        
+        <p class="landing-instructions text">Let's figure out how to customize drinks together!</p>
+        
     
+        <div class='martini-box'>
+        
+        <div class='whoosh'>Let's begin.</div>
+    </div>
+    </div>);
 }
+}
+
+export default App;
